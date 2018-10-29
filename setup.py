09097dac4d577c399c98b0a4dddbf44c5543fe41
sankalpsai <38495109+sankalpsai@users.noreply.github.com>
2018-10-29 14:08:07 +0530

    Update setup.py
    Changed the version to 0.0.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "CyberSourceRest"
-VERSION = "1.0.0"
+VERSION = "0.0.1"
 # To install the library, run the following
 #
 # python setup.py install
