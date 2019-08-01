e6660bc9cb3d9a8062abd2396416fa538a7a7ec0
Gabriel Broadwin Nongsiej <gnongsie@visa.com>
2019-08-01 12:45:13 +0530

    + Fixing issue with PaymentType.Method

--- CyberSource/apis/search_transactions_api.py
+++ CyberSource/apis/search_transactions_api.py
@@ -163,7 +163,7 @@ class SearchTransactionsApi(object):
         :param callback function: The callback function
             for asynchronous request. (optional)
         :param str search_id: Search ID. (required)
-        :return: TssV2TransactionsPost201Response1
+        :return: TssV2TransactionsPost201Response
                  If the method is called asynchronously,
                  returns the request thread.
         """
@@ -189,7 +189,7 @@ class SearchTransactionsApi(object):
         :param callback function: The callback function
             for asynchronous request. (optional)
         :param str search_id: Search ID. (required)
-        :return: TssV2TransactionsPost201Response1
+        :return: TssV2TransactionsPost201Response
                  If the method is called asynchronously,
                  returns the request thread.
         """
@@ -246,7 +246,7 @@ class SearchTransactionsApi(object):
                                         body=body_params,
                                         post_params=form_params,
                                         files=local_var_files,
-                                        response_type='TssV2TransactionsPost201Response1',
+                                        response_type='TssV2TransactionsPost201Response',
                                         auth_settings=auth_settings,
                                         callback=params.get('callback'),
                                         _return_http_data_only=params.get('_return_http_data_only'),
