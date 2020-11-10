6724c86e646830f8ce41fa88eefb7176e6446160
Gabriel Broadwin Nongsiej <gnongsie@visa.com>
2020-11-10 13:21:28 +0530

    Upgrade to new version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "cybersource-rest-client-python"
-VERSION = "0.0.21"
+VERSION = "0.0.22"
 # To install the library, run the following
 #
 # python setup.py install
