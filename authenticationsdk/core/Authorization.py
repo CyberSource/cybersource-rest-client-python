2cfc013692ab739cee0d7aa49ec98e39c0198fe5
snavinch <snavinch@visa.com>
2021-06-17 18:07:23 +0530

    - Removed logging Token

--- authenticationsdk/core/Authorization.py
+++ authenticationsdk/core/Authorization.py
@@ -58,13 +58,13 @@ class Authorization:
                     logger.info(GlobalLabelParameters.DATE + ":   " + date_time)
                     logger.info(GlobalLabelParameters.HOST + ":   " + mconfig.request_host)
                     # Logging the Digest when Request_type_method is Post
-                    logger.info("Authorization Bearer:     " + sig_token_jwt.encode("utf-8").decode("utf-8"))
+                    # logger.info("Authorization Bearer:     " + sig_token_jwt.encode("utf-8").decode("utf-8"))
                 return sig_token_jwt
             elif authentication_type.upper() == GlobalLabelParameters.OAUTH.upper():
                 token = OAuthToken()
                 token.oauth_token(mconfig)
                 o_auth_token = token.get_token() 
-                logger.info("Authorization Bearer:     " + o_auth_token.encode("utf-8").decode("utf-8"))
+                # logger.info("Authorization Bearer:     " + o_auth_token.encode("utf-8").decode("utf-8"))
                 return o_auth_token
             else:
                 raise ApiException(1, GlobalLabelParameters.AUTH_ERROR)
