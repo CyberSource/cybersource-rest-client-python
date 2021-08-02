e4af528245ac4a5f8572f558b7ccc40d79d67915
karthikeyan kumar <karthkum@visa.com>
2021-08-02 13:54:44 +0530

    adding new libs for token generation py

---
+++ CyberSource/utilities/flex/utils/transient_token_generator.py
@@ -0,0 +1,19 @@
+from . import base64
+from . import json
+
+
+class TransientTokenGenerator:
+    def __init__(self):
+        pass
+
+    def generate_transient_token(self,jwt):
+        split_contents[] = jwt.split(",")
+        if(len(split_contents) > 1):
+            encoded_string = split_contents[1]
+            data = base64.b64decode(encoded_string)
+            transient_model = json.load(data)
+            return transient_model
+
+        return none;    
+
+
