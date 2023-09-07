489a22cd635d279658734e68b92c0f4413239398
monu-kumar-visa <138751796+monu-kumar-visa@users.noreply.github.com>
2023-09-07 15:06:28 +0530

    added caching

--- authenticationsdk/util/JWEUtility.py
+++ authenticationsdk/util/JWEUtility.py
@@ -1,18 +1,14 @@
-from jwcrypto import jwe, jwk
+from jwcrypto import jwe
+
+from authenticationsdk.util.Cache import FileCache
 
 
 class JWEUtility:
     @staticmethod
-    def get_private_key_from_pem(pem_file_path):
-        with open(pem_file_path, 'r') as pem_file:
-            cert = pem_file.read()
-            private_key = jwk.JWK.from_pem(cert.encode('utf-8'))
-            return private_key
-
-    @staticmethod
     def decrypt_jwe_using_pem(merchant_config, jwe_base_64_data):
         jwe_token = jwe.JWE()
-        private_key = JWEUtility.get_private_key_from_pem(merchant_config.get_jwePEMFileDirectory())
-        jwe_token.deserialize(jwe_base_64_data, private_key)
+        cache_obj = FileCache()
+        cached_private_key = cache_obj.get_cached_private_key_from_pem(merchant_config.get_jwePEMFileDirectory(), 'NetworkTkenCert')
+        jwe_token.deserialize(jwe_base_64_data, cached_private_key)
         payload = jwe_token.payload
         return payload.decode('utf-8')
