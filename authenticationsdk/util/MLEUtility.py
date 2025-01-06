4589aae2b4ece4a7d3bf853bf6ec7749cfe2972d
gaubansa <gaubansa@visa.com>
2025-01-06 17:21:49 +0530

    adding mleUtility

---
+++ authenticationsdk/util/MLEUtility.py
@@ -0,0 +1,18 @@
+
+
+class MLEUtility:
+    @staticmethod
+    def check_is_mle_for_api(merchant_config, isMLESupportedByCybsForApi, operationId):
+        # isMLE for an api is false by default
+        isMLEForAPI = False
+
+        # return true or false based on logic 
+        return isMLEForAPI
+    
+    @staticmethod
+    def encrypt_request_payload(merchant_config, requestBody):
+        # complete the logic to encrypt request payload for mle    
+
+        # return encrypted requestBody
+        return requestBody
+
