be37dc82a3c9d52490cb79092d7527d4146cfa53
mahmishr <mahmishr@visa.com>
2025-01-15 10:26:33 +0530

    comments resolved

--- authenticationsdk/util/MLEUtility.py
+++ authenticationsdk/util/MLEUtility.py
@@ -7,7 +7,7 @@ class MLEUtility:
         if isMLESupportedByCybsForApi and merchant_config.get_useMLEGlobally():
             isMLEForAPI = True
         operation_array = [op_id.strip() for op_id in operationId.split(",")]
-        if merchant_config.get_mapToControlMLEonAPI() and merchant_config.get_mapToControlMLEonAPI():
+        if merchant_config.get_mapToControlMLEonAPI():
             for op_id in operation_array:
                 if op_id in merchant_config.get_mapToControlMLEonAPI():
                     isMLEForAPI = merchant_config.get_mapToControlMLEonAPI()[op_id]
