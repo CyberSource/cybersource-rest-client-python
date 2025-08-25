d9b79b0c7b1ec41c3ba4a6a20af4257a4a2dbcb5
Aastvik Goel <aastgoel@visa.com>
2025-08-25 15:44:12 +0530

    minor fix

--- authenticationsdk/util/MLEUtility.py
+++ authenticationsdk/util/MLEUtility.py
@@ -108,7 +108,7 @@ class MLEUtility:
                 break
 
         if not serial_number:
-            logger.warning(f"Serial number not found in MLE certificate for alias {merchant_config.mle_key_alias} in {merchant_config.p12_keyfilepath}.p12")
+            logger.warning(f"Serial number not found in MLE certificate for alias {merchant_config.mle_key_alias} in {merchant_config.p12KeyFilePath}")
             # Use the hex serial number from the certificate as fallback
             return format(certificate.serial_number, 'x')
 
